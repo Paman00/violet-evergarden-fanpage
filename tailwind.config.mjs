eaf91df1c494e41b8432e4ef4d6624f756c2100e
Ángel Garcia Beltrán <angelurano00@gmail.com>
2023-11-16 21:43:06 +0100

    Add characters page and update some components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,33 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 
-const contentHeight = "44vh";
+const contentHeight = '44vh';
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'EBGaramond': ['EB Garamond', 'serif'], 
-			},
-			backgroundImage: {
-				'left-top-header': "url('/frameTopLeft.png')",
-				'right-top-header': "url('/frameTopRight.png')",
-				'left-bottom-header': "url('/frameBottomLeft.png')",
-				'right-bottom-header': "url('/frameBottomRight.png')",
-			},
-			colors: {
-				'golden': "#b4832f",
-			},
-			spacing: {
-				'content-height': contentHeight,
-			},
-			minHeight: {
-				'content-height': contentHeight,
-			},
-			maxHeight: {
-				'content-height': contentHeight,
-			},
-		},
-	},
-	plugins: [],
-}
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                EBGaramond: ['EB Garamond', 'serif'],
+            },
+            backgroundImage: {
+                'left-top-header': "url('/frameTopLeft.png')",
+                'right-top-header': "url('/frameTopRight.png')",
+                'left-bottom-header': "url('/frameBottomLeft.png')",
+                'right-bottom-header': "url('/frameBottomRight.png')",
+            },
+            colors: {
+                golden: {
+                    DEFAULT: '#b4832f',
+                    light: '#b49258',
+                },
+                beige: {
+                    DEFAULT: '#efebd9',
+                },
+            },
+            spacing: {
+                'content-height': contentHeight,
+            },
+            minHeight: {
+                'content-height': contentHeight,
+            },
+            maxHeight: {
+                'content-height': contentHeight,
+            },
+        },
+    },
+    plugins: [],
+};
